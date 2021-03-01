5d07a55e9f89ca192ee35904fdf4c4d65f33b2c4
Zhenya <e.mezhueva@gmail.com>
2021-03-01 21:02:01 +0200

    markup finfshed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 
-function App() {
+const getTodos = () => fetch('https://jsonplaceholder.typicode.com/todos?_limit=8')
+  .then(response => response.json());
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  console.log(getTodos(), todos)
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  },[]);
+
+  const completeHandler = (id) => {
+    setTodos(todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        }
+      }
+      return todo;
+    }))
+  }
+
   return (
     <div className="App">
-      todo
+      <div className="wrap">
+        <nav className="nav">
+          <a href="#allTodos" className="nav__tab nav__tab--active">
+            <img src="/images/list.png" className="nav__img" alt="list-logo"/>
+          </a>
+          <a href="#completedTodos" className="nav__tab">
+          <img src="/images/complete.png" className="nav__img" alt="list-logo"/>
+        </a>
+        </nav>
+        <ul className="todo-list">
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <div className="todo">
+                <input
+                  type="checkbox"
+                  id={todo.id}
+                  checked={todo.completed}
+                  onChange={() => completeHandler(todo.id)}
+                />
+                <label
+                  htmlFor={todo.id}
+                  className="todo__label"
+                >
+                  <span className={todo.completed ? "todo__text todo__text--completed" : "todo__text"}>
+                    {todo.title}
+                  </span>
+                </label>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button className="btn icon-fill">+</button>
+      </div>
     </div>
   );
 }
